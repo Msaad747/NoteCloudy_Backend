38ec21b09af473bb59f4b3bfe6c89d8b53c9eb1e
Saad <saadhunter007@gmail.com>
2026-06-30 17:18:40 +0500

    Ready to Push

--- db.js
+++
@@ -1,8 +0,0 @@
-// import sqlite3 from "sqlite3";
-
-// const db = new sqlite3.Database("./database.db", (err) => {
-//   if (err) console.log(err.message);
-//   else console.log("Connected to SQLite");
-// });
-
-// export default db;
